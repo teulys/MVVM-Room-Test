bf7efeb0bfc16ea91573c0e2210cc54a0523be08
Teulys Jimenez <teulys@gmail.com>
2019-11-07 21:41:07 -0400

    Add component

--- app/src/main/java/com/diplomado/userroomsqlite/MainActivity.java
+++ app/src/main/java/com/diplomado/userroomsqlite/MainActivity.java
@@ -3,12 +3,25 @@ package com.diplomado.userroomsqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText tvName, tvLastname, tvFind;
+    private ListView lvUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        if (view.getId() == R.id.btSave) {
+            
+        }
     }
 }
